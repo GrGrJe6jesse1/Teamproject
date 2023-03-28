cd5300227a864bfaceff54fd3908e56f7a7cb167
GrGrJe6jesse1 <hp6sa@naver.com>
2023-03-28 16:18:56 +0900

    팀플스타트

--- dwteam/src/App.js
+++ dwteam/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NintendoCSA from './componentCSA/NintendoCSA'
+import {Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-
+      <NintendoCSA/>
     </div>
   );
 }

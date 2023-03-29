8cff75b07c5c6afd34b39ab7f5972ac9d9edd56b
GrGrJe6jesse1 <hp6sa@naver.com>
2023-03-29 17:26:08 +0900

    DB 작성중

--- dwteam/src/componentCSA/NintendoCSA.jsx
+++ dwteam/src/componentCSA/NintendoCSA.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import './NintendoCSA.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import poke from './poDLC.png'
+import { Container, Row, Col } from 'react-bootstrap'
+import axios from 'axios';
 
 
-const nintendoCSA = () => {
+const NintendoCSA = () => {
+  useEffect(() => {
+    axios.get('http://localhost:3000/products')
+      .then(response => {
+        console.log(response);
+      })
+  })
+
   return (
     <section className='nin-wrapper'>
       <div className="nin-wi">
+        {/* 웰컴이미지 - 몇가지 소프트타이틀 이미지 편집해서 생성 */}
+        {/* 호버 또는 클릭시 이미지 사라지면서 컨텐츠 화면으로 */}
         <img className='WELCOME' src={poke} alt="WELCOME" />
       </div>
       {
+        // DB로 돌릴 예정
         [1,2,3,4,5,6,7,8].map((num) =>{
           return (
             <div className="nin-sw-item">
@@ -20,15 +32,16 @@ const nintendoCSA = () => {
           )         
         })
       }
+      {/* 항목 박스 클릭시 모달 발생 */}
       <div className="nin-modal">
         <img className='modal-img1' src="" alt="" />
         <img className='modal-img2' src="" alt="" />
         <img className='modal-img3' src="" alt="" />
         <h2>게임 특징설명</h2>
-        <p></p>
+        <p>간단 소개문</p>
       </div>
     </section>
   )
 }
 
-export default nintendoCSA
+export default NintendoCSA

0b2306ea9027901d03b55d282bc0c8e7adbcf2e7
GrGrJe6jesse1 <hp6sa@naver.com>
2023-03-30 17:34:47 +0900

    웰컴이미지 예시 작성

--- dwteam/src/componentCSA/NintendoCSA.jsx
+++ dwteam/src/componentCSA/NintendoCSA.jsx
@@ -1,45 +1,71 @@
 import React from 'react'
 import './NintendoCSA.css'
 import { useEffect, useState } from 'react'
-import poke from './poDLC.png'
+import WIEXE from './welcome-exe.gif'
+import SAMUS from './WI-Samus-.png'
+import SSBU from './WI-SSBU.webp'
+import Link from './WI-Link.webp'
+import MARIO from './WI-MARIO.webp'
+import Alear from './WI-Alear-.png'
+import Inkling from './WI-Inkling-.png'
+import Kirby from './WI-Kirby.png'
+import WILOGO from './logo_nin.gif'
+import villager from './WI-villager.png'
+import pikachu from './WI-pikachu-.png'
 import { Container, Row, Col } from 'react-bootstrap'
 import axios from 'axios';
 
 
 const NintendoCSA = () => {
-  useEffect(() => {
-    axios.get('http://localhost:3000/products')
-      .then(response => {
-        console.log(response);
-      })
-  })
+  // useEffect(() => {
+  //   axios.get('http://localhost:3000/products')
+  //     .then(response => {
+  //       console.log(response);
+  //     })
+  // })
 
   return (
     <section className='nin-wrapper'>
+      {/* 임시 */}
+      <img src={WIEXE} alt="WELCOME" />
+
       <div className="nin-wi">
         {/* 웰컴이미지 - 몇가지 소프트타이틀 이미지 편집해서 생성 */}
-        {/* 호버 또는 클릭시 이미지 사라지면서 컨텐츠 화면으로 */}
-        <img className='WELCOME' src={poke} alt="WELCOME" />
+        {/* 디자인 수정 웰컴이미지 정가운데 로고를 중심으로 독점타이틀 캐릭터들 바깥에서 */}
+        {/* 안쪽으로 들어오는 애니메이션 효과 */}
+        {/* 클릭시 각 시리즈 소개 화면으로 */}
+        <img className='WELCOME-SAMUS' src={SAMUS} alt="SAMUS" />
+        <img className='WELCOME-SSBU' src={SSBU} alt="SSBU" />
+        <img className='WELCOME-Link' src={Link} alt="Link" />
+        <img className='WELCOME-MARIO' src={MARIO} alt="MARIO" />
+        <img className='WELCOME-Alear' src={Alear} alt="Alear" />
+        <img className='WELCOME-Inkling' src={Inkling} alt="Inkling" />
+        <img className='WELCOME-Logo' src={WILOGO} alt="LOGO-NIN" />
+        <img className='WELCOME-Kirby' src={Kirby} alt="Kirby" />
+        <img className='WELCOME-villager' src={villager} alt="villager" />
+        <img className='WELCOME-pikachu' src={pikachu} alt="pikachu" />
       </div>
       {
         // DB로 돌릴 예정
-        [1,2,3,4,5,6,7,8].map((num) =>{
-          return (
-            <div className="nin-sw-item">
-              <img src="https://www.nintendo.co.kr/front_images/news/1241/4a54c9f3888fa612cd0dacfcab000c97.jpg" alt="zelda" />
-              <h2>Titie</h2>
-            </div>
-          )         
-        })
+        // 수정/ map함수로 모달또는 페이지 생성 DB 불러오기
+        // [1,2,3,4,5,6,7,8].map((num) =>{
+        //   return (
+        //     <div className="nin-sw-item">
+        //       <img src="https://www.nintendo.co.kr/front_images/news/1241/4a54c9f3888fa612cd0dacfcab000c97.jpg" alt="zelda" />
+        //       <h2>Titie</h2>
+        //     </div>
+        //   )         
+        // })
       }
       {/* 항목 박스 클릭시 모달 발생 */}
-      <div className="nin-modal">
+      {/* 디자인 수정으로 보류 */}
+      {/* <div className="nin-modal">
         <img className='modal-img1' src="" alt="" />
         <img className='modal-img2' src="" alt="" />
         <img className='modal-img3' src="" alt="" />
         <h2>게임 특징설명</h2>
         <p>간단 소개문</p>
-      </div>
+      </div> */}
     </section>
   )
 }
